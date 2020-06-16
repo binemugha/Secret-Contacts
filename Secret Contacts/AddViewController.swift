b65bf2bf2bbbd42c898d333684efd0e499388fc5
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-16 10:49:26 +0100

    Add ViewController to fill up TableViewCell

---
+++ Secret Contacts/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  Secret Contacts
+//
+//  Created by Benjamin Inemugha on 16/06/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
