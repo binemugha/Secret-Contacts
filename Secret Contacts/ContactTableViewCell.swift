b833f3a35c7f9b6b6fd369b89b33b35435890b80
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-16 08:24:01 +0100

    Added Table View Controller and Cell file

---
+++ Secret Contacts/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  Secret Contacts
+//
+//  Created by Benjamin Inemugha on 16/06/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
