dd852b5219e5d8ed74563a9718fac93cfe98af94
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-17 20:26:00 +0100

    UI Improvement

--- Secret Contacts/ContactTableViewController.swift
+++ Secret Contacts/ContactTableViewController.swift
@@ -77,17 +77,22 @@ class ContactTableViewController: UITableViewController,  NSFetchedResultsContro
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        let managedObject : NSManagedObject = frc.object(at: indexPath) as! NSManagedObject
+        pc.delete(managedObject)
+        
+        do{
+            try pc.save()
+        }
+        
+        catch{
+            print(error)
+            return
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
